e5a9f274de6d830e8a1aaf0d182e46fe0b275c86
Wayne Van Son <waynevanson@gmail.com>
2024-02-23 18:46:52 +1100

    chore: structure cli

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,40 @@
-use std::path::PathBuf;
-
 use clap::Parser;
+use std::path::PathBuf;
 
 #[derive(Debug, Clone, Parser)]
 struct Args {
     source: PathBuf,
-    target: PathBuf,
-    prefix: PathBuf,
+
+    /// Defaults to the platform specific `$HOME/.dotfiles/`.
+    #[arg()]
+    store: Option<PathBuf>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Parser, Clone)]
+enum Commands {
+    Apply,
+    Update,
+    Add {
+        /// Path to the file or directory that should be saved to the dotfiles
+        target: PathBuf,
+        /// The Defaults to the store.
+        sub_store: Option<PathBuf>,
+    },
+    Remove {
+        /// Path to the file or directory that should be removes from the dotfiles
+        target: PathBuf,
+        /// Defaults to the store.
+        sub_store: Option<PathBuf>,
+    },
 }
 
 fn main() {
-    let mut args = Args::parse();
+    let args = Args::parse();
+    let store = args
+        .store
+        .or_else(|| home::home_dir())
+        .expect("Unable to find the store directory automatically. Please explicitly set it by using the flag '--store' or '-s'");
 }
